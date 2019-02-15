b8165fe4e7bebb5cfe00c5d26f0a51a7fb910495
github username <suoarik@gmail.com>
2019-02-15 21:46:34 +0000

    fetch geocode api data

---
+++ playground/callbacks.js
@@ -0,0 +1,14 @@
+// example of async callback
+const getUser = (id, callback) => {
+    const user = {
+        id: id,
+        name: "Vikram"
+    }
+    setTimeout(() => {
+    callback(user)
+    }, 3000)
+}
+
+getUser(31, (user) => {
+    console.log(user)
+})
