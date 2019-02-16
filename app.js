46718daf761de4527134acd12f5bc7a80e7b887e
github username <suoarik@gmail.com>
2019-02-16 15:49:03 +0000

    cleaned api calls into correct folders, added a dynamic weather apu tempreture call

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-const request = require("request")
 const config = require("./config")
-const yargs = require('yargs')
+// const yargs = require('yargs')
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: "address",
-            describe: "Address to fetch weather for",
-            // tells yargs the arguments to always be a string
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv
 
-let addressUrl = encodeURIComponent(argv.a)
+// const geocode = require('./geocode/geocode')
 
-request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${config.key}&location=${addressUrl}`,
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: "address",
+//             describe: "Address to fetch weather for",
+//             // tells yargs the arguments to always be a string
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv
+
+
+//     console.log(geocode)
+// geocode.geocodeAddress(argv.a, (errorMessage, result) => {
+//     if (errorMessage) {
+//         console.log(errorMessage)
+//     } else {
+//         console.log(JSON.stringify(result, undefined, 2))
+//     }
+// })
+
+const request = require('request')
+
+const lat = 43.3
+const long = 54.4
+
+return request({
+    url: `https://api.darksky.net/forecast/${config.darkSKy}/${lat},${long}`,
     json: true
 }, (error, response, body) => {
-     if (error) {
-         console.log("Unable to connect to Google Servers")
-     } else if (body.info.statuscode === 400) {
-         console.log("Unable to find that Address")
-     } else if (response.statusCode === 200){
-        console.log(JSON.stringify(response, undefined, 2))
-
-        console.log(body.results[0].locations[0].latLng.lat)
-        console.log(body.results[0].locations[0].latLng.lng)
-     }
-})
+        if (!error && response.statusCode === 200) {
+            console.log(body.currently.temperature)
+        } else {
+            console.log("Unable to fetch weather.")
+        } 
+    })
 
 
 
