26af34987b24befcc67746234ae706c7d696dea4
github username <suoarik@gmail.com>
2019-02-16 14:08:26 +0000

    extract locations from mapquestapi

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const request = require("request")
 const config = require("./config")
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${config.key}`,
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=${config.key}&location=1301%20lombard%20street%20philadelphia`,
     json: true
 }, (error, response, body) => {
-    console.log(body)
+    // allows us to pretty print the object in the console
+    // what to stringify, filter, indentations
+    console.log(JSON.stringify(body, undefined, 2))
+
+        console.log(body.results[0].locations[0].latLng.lat)
+
+        console.log(body.results[0].locations[0].latLng.lng)
 })
 
-console.log(config)
+
+
+
