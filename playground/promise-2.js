51b6d58363697e77058b1889d6de48d18adb85e3
github username <suoarik@gmail.com>
2019-02-18 22:04:14 +0000

    upgraded weather app with promises

---
+++ playground/promise-2.js
@@ -0,0 +1,38 @@
+const request = require('request')
+const config = require('../config')
+
+const geocodeAddress = (address) => {
+    return new Promise((resolve, reject) => {
+        let addressUrl = encodeURIComponent(address)
+
+        return request({
+            url: `http://www.mapquestapi.com/geocoding/v1/address?key=${config.key}&location=${addressUrl}`,
+            json: true
+        }, (error, response, body) => {
+
+            if (error) {
+                reject("Unable to connect to Google Servers")
+            } else if (body.info.statuscode === 400) {
+                reject("Unable to find that Address")
+            } else if (response.statusCode === 200) {
+                resolve({
+
+                    address: address,
+                    latitude: body.results[0].locations[0].latLng.lat,
+                    longitude: body.results[0].locations[0].latLng.lng
+                })
+                // console.log(JSON.stringify(body, undefined, 2))
+            }
+        })
+    })
+}
+
+
+geocodeAddress('UB6 8SX')
+    .then((resp) => {
+        console.log(JSON.stringify(resp, undefined, 2))
+    }, (error) => {
+        console.log(error)
+    })
+
+
--- playground/promise.js
+++ playground/promise.js
@@ -1,8 +1,20 @@
+const asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // type of
+            if (typeof(a) === 'number' && typeof(b) === 'number') {
+                resolve(a + b)
+            } else {
+                reject('arguments must be numbers')
+            }
+        }, 1500)
+    })
+}
 
 const somePromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve('Hey it worked')
-        reject('Unable to fulfill promise')
+        resolve('Hey it worked')
+        // reject('Unable to fulfill promise')
     }, 2500)
 
 })
@@ -10,3 +22,12 @@ const somePromise = new Promise((resolve, reject) => {
 somePromise
     .then(message => console.log(message), (errorMessage => console.log({error: errorMessage})))
     // .catch(message => console.log(message))
+
+asyncAdd(5, "7")
+    .then((resp) => {
+        console.log("Result", resp)
+        return asyncAdd(resp, 33)
+    }).then((res) => {
+        console.log("Should be 45", res)
+    })
+    .catch((error) => console.log(error))
